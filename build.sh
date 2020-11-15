2df83a97a2b45139d770fbb1c04d81b5bfdce6a3
Pascal Geiser <pgeiser@pgeiser.com>
2020-11-15 17:00:31 +0100

    Use my shared bash-helpers scripts

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -ex
+source bash-helpers/helpers.sh
+docker_setup "jpeg-recompress-appimage"
+dockerfile_create
+dockerfile_appimage
+dockerfile_switch_to_user
+docker_build_image_and_create_volume
+cat >>$DOCKERFILE <<'EOF'
+WORKDIR /work
+RUN set -ex \
+    && git clone https://github.com/mozilla/mozjpeg.git \
+    && mkdir -p build_mozjpeg \
+    && cd build_mozjpeg \
+    && cmake -G"Unix Makefiles" -DPNG_SUPPORTED=0 -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=/usr/lib ../mozjpeg \
+    && make -j install DESTDIR=/work/AppDir
+RUN set -ex \
+    && git clone https://github.com/danielgtaylor/jpeg-archive.git \
+    && cd jpeg-archive \
+    && make -j MOZJPEG_PREFIX=/work/AppDir/usr LIBJPEG=/work/AppDir/usr/lib/libjpeg.a \
+    && make install PREFIX=/work/AppDir
+COPY jpeg-archive.desktop /work/AppDir/jpeg-archive.desktop
+RUN set -ex \
+    && rm -rf /work/AppDir/usr/ \
+    && rm -f /work/AppDir/bin/jpeg-archive \
+    && rm -f /work/AppDir/bin/jpeg-hash \
+    && rm -f /work/AppDir/bin/jpeg-compare \
+    && find /work/AppDir \
+    && chmod a+x /work/AppDir/AppRun
+RUN set -ex \
+    && export LD_LIBRARY_PATH=/work/AppDir/usr/lib/ ; find /work/AppDir/ -type f -executable -exec ldd {} \; | grep "not found" | true \
+    && cp /usr/share/icons/Adwaita/scalable/mimetypes/application-x-executable-symbolic.svg /work/AppDir \
+    && ./appimagetool-x86_64.AppImage --appimage-extract-and-run AppDir
+RUN set -ex \
+    && cd /work/AppDir \
+    && strace ./AppRun || true
+EOF
+docker_build_image_and_create_volume
+#run_shfmt_and_shellcheck
+#$DOCKER_RUN_IT "$@"
+if [ $# -eq 0 ]; then
+	$DOCKER_RUN_I cp jpeg-recompress-x86_64.AppImage /mnt
+else
+	$DOCKER_RUN_I "$@"
+fi
--- docker_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-docker rm -f jpeg-archive || true
-docker build -t jpeg-archive .
-docker run --rm --entrypoint cat jpeg-archive /jpeg-recompress-x86_64.AppImage > jpeg-recompress-x86_64.AppImage
-chmod +x ./jpeg-recompress-x86_64.AppImage
